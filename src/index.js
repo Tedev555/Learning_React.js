a80ffb70a22f404e44c82373865afd9503590d42
ted555 <thanong984@gmail.com>
2018-02-06 22:34:20 +0700

    Components and Props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Rendering Elements</h1>
-            <h2>It is {''} {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    )
+//Define component with Function 
+function Welcome(props) {
 
- ReactDOM.render(element, document.getElementById('root'));
+    return <h1>Hello, {props.name}</h1>
 }
 
-setInterval(tick, 1000)
+//Components can refer to other components in their output.
+//Components can reuseable
+function App() {
+
+    return(
+        <div>
+            <Welcome name="Ted"/>
+            <Welcome name="Kanya"/>
+            <Welcome name="Khamphet"/>
+        </div>
+    )
+}
+const element = <App />
+
+ ReactDOM.render(element, document.getElementById('root'));
