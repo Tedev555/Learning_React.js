b661ad6bd33fda2a0d9d1b6b9ea263f3f289fde7
ted555 <thanong984@gmail.com>
2018-02-06 22:10:54 +0700

    How Rendering Elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function tick() {
+    const element = (
+        <div>
+            <h1>Rendering Elements</h1>
+            <h2>It is {''} {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    )
+
+ ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000)
